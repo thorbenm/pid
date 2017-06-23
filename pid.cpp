ccb11fb63285301f5058d8d9bb6e3e823460b580
Thorben B. Muehleder <thorbenm@me.com>
2017-06-23 10:39:32 +0200

    now there is a constructor

--- pid.cpp
+++ pid.cpp
@@ -1,7 +1,14 @@
 #include "pid.h"
 
+pid::pid(double kprop2, double kint2, double kdiff2){
+	kprop = kprop2;
+	kint  = kint2 ;
+	kdiff = kdiff2;
+}
+
 void pid::read_value(double value){
 	error = value - goal;
 	int_error += error;
 	diff_error = error - prev_error;
 }
+
--- pid.h
+++ pid.h
@@ -1,10 +1,14 @@
 class pid{
+public:
+	void read_value(double value = 0);
+	pid(double kprop2 = 1, double kint2 = 0, double kdiff2 = 0);
 private:
+	double kprop;
+	double kint;
+	double kdiff;
 	double goal;
 	double error;
 	double int_error;
 	double diff_error;
 	double prev_error;
-public:
-	void read_value(double value = 0);
 };
