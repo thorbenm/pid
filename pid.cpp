8dac5b20f93e7e743a3cde5c31bdb8de3ccf88da
Thorben B. Muehleder <thorbenm@physik.uni-kassel.de>
2017-06-29 10:28:04 +0000

    added the time dt

--- pid.cpp
+++ pid.cpp
@@ -1,17 +1,19 @@
 #include "pid.h"
 
-pid::pid(double kprop2, double kint2, double kdiff2){
+pid::pid(double kprop2, double kint2, double kdiff2, double dt2){
 	kprop = kprop2;
 	kint  = kint2 ;
 	kdiff = kdiff2;
+	dt = dt2;
 	prev_error = 0;
 }
 
 double pid::update(double input){
 	error = goal - input;
-	int_error += error;
-	diff_error = error - prev_error;
+	int_error += error * dt;
+	diff_error = (error - prev_error) / dt;
 	value = kprop * error + kint * int_error + kdiff * diff_error;
+	prev_error = error;
 	return value;
 }
 
--- pid.h
+++ pid.h
@@ -5,11 +5,12 @@ public:
 	void set_value(double input = 0);
 	void set_goal(double input = 0);
 	double update(double input = 0);
-	pid(double kprop2 = 1, double kint2 = 0, double kdiff2 = 0);
+	pid(double kprop2 = 1, double kint2 = 0, double kdiff2 = 0, double dt2 = 1);
 private:
 	double kprop;
 	double kint;
 	double kdiff;
+	double dt;
 	double goal;
 	double value;
 	double error;
